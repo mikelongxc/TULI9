aca31c41c52df3f047c19038abd66e8617620366
Michael Long <mikelongxc@gmail.com>
2022-03-09 21:18:09 -0800

    ∆

--- TULI9.nim
+++ TULI9.nim
@@ -6,14 +6,18 @@ type
       sym: string
 
    AppC = ref object of ExprC
+      fun: ExprC
+      args: array
 
    CondC = ref object of ExprC
+      ifCond: ExprC
+      thenCond: ExprC
+      elseCond: ExprC
 
    LamC = ref object of ExprC
+      parms: array
+      body: ExprC
 
-# Typedef for Env
-type
-   Env = ref object of RootObj
 
 # Typedef for a Value
 type
@@ -21,22 +25,40 @@ type
    
    NumV = ref object of Value
       num: int
+
    StrV = ref object of Value
       str: string
+
    BoolV = ref object of Value
       b: bool
+
+   PrimV = ref object of Value
+      op: proc
+
+
+   NullV = ref object of Value
+
+
+
+# Typedef for a Environment (and CloV)
+type
+   Env = ref object of RootObj
+      next: Env
+      name: string
+      val: Value
+
    CloV = ref object of Value
       parms: array
       body: ExprC
       env: Env
-   PrimV = ref object of Value
-      op: proc
 
-   NullV = ref object of Value
 
-# Looks up symbol in an environment
-proc LookupEnv(env : Env, sym : IdC) : Value =
-   return NumV(num: 1)
+# Lookup function utilizing our Env
+proc lookup(env : Env, sym : IdC) : Value =
+   if env.name == sym.sym:
+      return env.val
+   else:
+      lookup(env.next, sym)
 
 
 # Beginning of the interp function
@@ -48,21 +70,24 @@ proc interp(exp : ExprC, env : Env) : Value =
    elif exp of StrV:
       return StrV(exp)
    elif exp of IdC:
-      return LookupEnv(env, IdC(exp))
+      return NullV() #lookup(env, IdC(exp))
    elif exp of AppC:
       return NullV()
    elif exp of CondC:
       return NullV()
    elif exp of LamC:
       return NullV()
-   
 
-# Test Cases
 
 var topEnv: Env
-topEnv = Env()
-
+topEnv = Env(next: nil, name: "hello", val: BoolV(b : true))
+#[
+# Interp Test Cases
 assert(NumV(interp(NumV(num: 5), topEnv)).num == 5)
 assert(BoolV(interp(Boolv(b: true), topEnv)).b)
 assert(not BoolV(interp(Boolv(b: false), topEnv)).b)
-assert(StrV(interp(StrV(str: "Hello World"), topEnv)).str == "Hello World")
+]#
+
+# Env Lookup Test Cases
+let testEnv1 = Env(next: nil, name: "hello", val: BoolV(b : true))
+assert(BoolV(lookup(testEnv1, IdC(sym: "hello"))).b)
