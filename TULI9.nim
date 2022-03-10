68156d9d4d86cacfcf1ff93ded243496c9a7f3ca
Michael Srouji <miky.srouji@gmail.com>
2022-03-09 20:14:35 -0800

    Made ExprC typedef

--- TULI9.nim
+++ TULI9.nim
@@ -1,14 +1,17 @@
+# Typedef for a ExprC
+type
+   ExprC = ref object of RootObj
+
 # Typedef for a Value
 type
-   Value = ref object of RootObj
-      name: string
+   Value = ref object of ExprC
 
    NumV = ref object of Value
-      num: int
+      num*: int
 
 # Beginning of the interp function
-proc interp(exp : Value) =
+proc interp(exp : ExprC) : Value =
    if exp of NumV:
-      echo "NumV"
+      return NumV(exp)
 
-interp(NumV(num: 5))
+discard interp(NumV(num: 5))
