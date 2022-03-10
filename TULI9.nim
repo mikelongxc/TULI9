5b148874d3863d14cc8a5c90eb674ed6dcdfea66
Michael Srouji <miky.srouji@gmail.com>
2022-03-09 20:34:15 -0800

    Fleshed out some more Values, and created our first real test case

--- TULI9.nim
+++ TULI9.nim
@@ -6,12 +6,23 @@ type
 type
    Value = ref object of ExprC
 
-   NumV* = ref object of Value
-      num*: int
+   NumV = ref object of Value
+      n: int
+
+   BoolV = ref object of Value
+      b: bool
+
+   StrV = ref object of Value
+      s: string
 
 # Beginning of the interp function
 proc interp(exp : ExprC) : Value =
    if exp of NumV:
       return NumV(exp)
+   elif exp of BoolV:
+      return BoolV(exp)
+   elif exp of StrV:
+      return StrV(exp)
 
-echo NumV(interp(NumV(num: 5))).num
+# Test Cases
+assert(NumV(interp(NumV(n: 5))).n == 5)
