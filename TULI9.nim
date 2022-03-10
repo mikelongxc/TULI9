67bf99ad350af3380d2900746ab512f134c2b8da
Michael Long <mikelongxc@gmail.com>
2022-03-09 20:34:59 -0800

    values and such

--- TULI9.nim
+++ TULI9.nim
@@ -1,25 +1,30 @@
-# Typedef for a Value
-type
-   Value = ref object of RootObj
-      name: string
-
-   NumV = ref object of Value
-      num: int
-
+# Typedef for a ExprC
 type
    ExprC = ref object of RootObj
-      name: string
 
-   NumC = ref object of ExprC
-      num: int
-   StrC = ref object of ExprC
-      num: string
-   IdC = ref object of ExprC
-      num: string
+type
+   Env = ref object of RootObj
+
+# Typedef for a Value
+type
+   Value = ref object of ExprC
+   
+   NumV = ref object of Value
+      num*: int
+   StrV = ref object of Value
+      str*: string
+   BoolV = ref object of Value
+      v*: bool
+   CloV = ref object of Value
+      parms*: array
+      body*: ExprC
+      env*: Env
+   PrimV = ref object of Value
+      op*: proc
 
 # Beginning of the interp function
-proc interp(exp : Value) =
+proc interp(exp : ExprC) : Value =
    if exp of NumV:
-      echo "NumV"
+      return NumV(exp)
 
-interp(NumV(num: 5))
+discard interp(NumV(num: 5))
